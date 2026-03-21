a7e133d88e97c9da683152b0bb37884ead7f81f3
HassiyYT <81773493+HassiyYT@users.noreply.github.com>
2026-03-21 08:37:24 +0000

    chore: update donutbrowser to version 0.17.6

--- package.nix
+++ package.nix
@@ -9,14 +9,14 @@
 
 let
   pname = "donutbrowser";
-  version = "0.17.5";
+  version = "0.17.6";
 
   # Updated automatically by scripts/update-version.sh
-  assetName = "Donut_0.17.5_amd64.AppImage";
+  assetName = "Donut_0.17.6_amd64.AppImage";
 
   src = fetchurl {
     url = "https://github.com/zhom/donutbrowser/releases/download/v${version}/${assetName}";
-    hash = "sha256-c0B5qg0jDw3Hzj580X3zs60CeLQFmhUkZniFOnh8xHs=";
+    hash = "sha256-Bmqmb0zgaC02DKC9gcyI/St9wfwFWTEoYybOb1LXiS0=";
   };
 
   appimageContents = appimageTools.extractType2 {
